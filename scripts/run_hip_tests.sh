0d20fc7128a0fe4e35fe5be0841cb7695fdeec59
Debasis Mandal <Debasis.Mandal@amd.com>
2025-11-14 11:58:43 -0500

    PyTest script for single prefill (#48)
    A PyTest script to validate single prefill. This can be run locally or
    through CI. Currently, it tests the following configurations
    
    ```python
    @pytest.mark.parametrize("qo_len", [37, 17, 127, 577])
    @pytest.mark.parametrize("kv_len", [54, 97, 512, 2048])
    @pytest.mark.parametrize("num_qo_heads", [4, 32])
    @pytest.mark.parametrize("num_kv_heads", [4])
    @pytest.mark.parametrize("head_dim", [64])
    @pytest.mark.parametrize("kv_layout", ["NHD", "HND"])
    @pytest.mark.parametrize("causal", [False])
    @pytest.mark.parametrize("pos_encoding_mode", ["NONE"])
    @pytest.mark.parametrize("logits_soft_cap", [0.0, 8.0])
    @pytest.mark.parametrize("return_lse", [False, True])
    ```

--- scripts/run_hip_tests.sh
+++ scripts/run_hip_tests.sh
@@ -2,12 +2,14 @@
 #
 # SPDX - License - Identifier : Apache 2.0
 
-python -m pytest ../tests/test_sliding_window_hip.py \
-../tests/test_batch_decode_kernels_hip.py \
+python -m pytest \
 ../tests/test_batch_decode_kernels_hip_fp8.py \
+../tests/test_batch_decode_kernels_hip.py \
 ../tests/test_batch_decode_vllm.py \
-../tests/test_rope.py \
-../tests/test_page.py \
-../tests/test_norm_hip.py \
 ../tests/test_logits_cap_hip.py \
 ../tests/test_non_contiguous_decode_hip.py \
+../tests/test_norm_hip.py \
+../tests/test_page.py \
+../tests/test_rope.py \
+../tests/test_single_prefill_kernels_hip.py \
+../tests/test_sliding_window_hip.py \
