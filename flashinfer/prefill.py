dd7c37e6ec40727d2c5b8ee1a45262fcbccfbf00
Debasis Mandal <Debasis.Mandal@amd.com>
2025-11-18 18:39:02 -0500

    Batch prefill example script (#58)
    Standalone script to test the e2e both the paged and the ragged batch
    prefill. Currently, only ragged batch prefill is hipified, but not the
    paged batch prefill. This script will be used to iteratively develop and
    test the hipified paged batch prefill.
    
    Note that the way the JIT for batch prefill is set up is that we need to
    have both paged and ragged batch prefill working for the JIT as they are
    part of the same single module. Hence the JIT compilation fails even
    only with ragged batch prefill now fails.

--- flashinfer/prefill.py
+++ flashinfer/prefill.py
@@ -75,6 +75,7 @@ def get_single_prefill_module(backend):
                     run_func = _kernels.single_prefill_with_kv_cache.default
                 else:
                     _kernels_sm90 = torch.ops.flashinfer_kernels_sm90
+
                     run_func = _kernels_sm90.single_prefill_with_kv_cache_sm90.default
             else:
                 module = gen_single_prefill_module(backend, *args).build_and_load()
@@ -173,7 +174,7 @@ def get_batch_prefill_module(backend):
             uri = get_batch_prefill_uri(backend, *args)
             if has_prebuilt_ops and uri in prebuilt_ops_uri:
                 if backend == "fa2":
-                    _kernels = torch.ops.flashinfer_kernels
+                    _kernels = torch.ops.flashinfer_hip_kernels
 
                     plan_func = _kernels.batch_prefill_with_kv_cache_plan.default
                     ragged_run_func = (
@@ -195,7 +196,7 @@ def get_batch_prefill_module(backend):
                         _kernels_sm90.batch_prefill_with_paged_kv_cache_sm90_run.default
                     )
             else:
-                module = gen_batch_prefill_module(backend, *args)
+                module = gen_batch_prefill_module(backend, *args).build_and_load()
                 plan_func = module.plan.default
                 ragged_run_func = module.ragged_run.default
                 paged_run_func = module.paged_run.default
