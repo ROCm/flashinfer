ccca0a9765c4a507f807aa91dcd1bc8472fd68e8
Diptorup Deb <diptorup.deb@amd.com>
2026-02-02 21:05:39 +0000

    Refactor jit.utils to match v0.3.0

--- flashinfer/jit/utils.py
+++ flashinfer/jit/utils.py
@@ -1,18 +1,7 @@
-"""
-Copyright (c) 2024 by FlashInfer team.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# SPDX-FileCopyrightText: 2024-2026 FlashInfer team.
+# SPDX-FileCopyrightText: 2025-2026 Advanced Micro Devices, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 import pathlib
 
@@ -30,32 +19,6 @@ def write_if_different(path: pathlib.Path, content: str) -> None:
         f.write(content)
 
 
-def parallel_load_modules(
-    load_module_func_args: List[Tuple[Callable, List[Any]]],
-):
-    threads = []
-    exceptions = []
-
-    def wrapper(func, args):
-        try:
-            func(*args)
-        except Exception as e:
-            exceptions.append((func, e))
-
-    for func, args in load_module_func_args:
-        thread = threading.Thread(target=wrapper, args=(func, args))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    if exceptions:
-        for func, e in exceptions:
-            print(f"Exception occurred in {func.__name__}: {e}")
-        raise RuntimeError("One or more exceptions occurred during module loading")
-
-
 dtype_map = {
     torch.float16: "half",
     torch.bfloat16: "nv_bfloat16",
@@ -67,6 +30,19 @@ dtype_map = {
     torch.uint32: "uint32_t",
     torch.int64: "int64_t",
     torch.uint64: "uint64_t",
+}
+
+dtype_cutlass_map = {
+    torch.float16: "cutlass::half_t",
+    torch.bfloat16: "cutlass::bfloat16_t",
+    torch.float8_e4m3fn: "cutlass::float_e4m3_t",
+    torch.float8_e5m2: "cutlass::float_e5m2_t",
+    torch.int8: "cutlass::int8_t",
+    torch.uint8: "cutlass::uint8_t",
+    torch.int32: "cutlass::int32_t",
+    torch.uint32: "cutlass::uint32_t",
+    torch.int64: "cutlass::int64_t",
+    torch.uint64: "cutlass::uint64_t",
 }
 
 dtype_map_hip = {
@@ -85,8 +61,8 @@ dtype_map_hip = {
 filename_safe_dtype_map = {
     torch.float16: "f16",
     torch.bfloat16: "bf16",
-    torch.float8_e4m3fnuz: "e4m3fnuz",
-    torch.float8_e5m2fnuz: "e5m2fnuz",
+    torch.float8_e4m3fn: "e4m3",
+    torch.float8_e5m2: "e5m2",
     torch.int8: "i8",
     torch.uint8: "u8",
     torch.int32: "i32",
